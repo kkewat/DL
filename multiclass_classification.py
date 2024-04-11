5483dff88b7187d86335e6954ada73a2b59b6004
kunal kewat <60972748+kkewat@users.noreply.github.com>
2024-04-11 13:29:57 +0530

    Add files via upload

---
+++ multiclass_classification.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+
+X,y = make_blobs(n_samples=100,centers=2,n_features=2,random_state=1)
+
+scaler.fit(X)
+X = Scaler.transform(X)
+
+model = Sequential()
+model.add(Dense(units=4,input_dim=2,activation='relu'))
+model.add(Dense(units=4,activation='relu'))
+model.add(Dense(units=2,activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.summary()
+
+model.fit(X,y, epochs=100)
+
+test_X,test_y = make_blob(n_samples=5,centers=2,n_features=2,random_state=20)
+prediction = model.predict(test_X)
+
+for i in range(5):
+    print(test_X[i],"\t",prediction[i],"\t",test_y[i])
